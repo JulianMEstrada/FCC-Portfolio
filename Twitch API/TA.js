9e315907eb3f8a7690098d8173d7f61cd1d41a4e
Julian Estrada <jestra11@ucsc.edu>
2017-06-27 19:35:38 -0700

    Update TA.js

--- Twitch API/TA.js
+++ Twitch API/TA.js
@@ -1,24 +1,23 @@
-
 //Jquery
 $(document).ready(function(){
 
 // Twitch Users
-var users = ["ESL_SC2","brunofin","comster404","OgamingSC2","cretetion","freecodecamp","storbeck","habathcx","RobotCaleb","noobs2ninjas","ShoutFactoryTV","StreamerHouse"];
+var users = ["ESL_SC2","brunofin","comster404","OgamingSC2","cretetion","storbeck","habathcx","RobotCaleb","noobs2ninjas","ShoutFactoryTV","StreamerHouse"];
 
-// Curent Online / Offline 
-var current;
+// Curent Online / Offline freecodecamp 
 
 // Current for loop and data
-for (var i = 0;i<users.length; i++) {
 
-var url2 = 'https://wind-bow.glitch.me/twitch-api/streams/'+users[i];
+var url2 = 'https://wind-bow.glitch.me/twitch-api/streams/freecodecamp';
 
 $.getJSON(url2,function(data2){
 
-if (data2.stream===null) {current='Currently Offline! ';}
-else {current='Currently Live! ';}
+if (data2.stream===null)
+$('#freecodecamp').html('FreeCodeCamp is Currently Offline! ');
 
-});}// DATA 2 and URL 2
+else {$('#freecodecamp').html('FreeCodeCamp is Currently Online! ');}
+
+});// DATA 2 and URL 2
 
 // If user does not exist below data will display.
 for (var i = 0;i<users.length; i++) {
@@ -34,7 +33,7 @@ logo="img/Twitch.png"
 name= data0.message;
 status = "No longer exist! "
 
-$("#userInfo").prepend("<div class = 'row'>"+"<div class='col'>"+"<a target='_blank' href="+link+"><img src=' "+logo+"'alt='img/Twitch.png'</a>"+"</div>"+"<div class='col'>"+name+"</div>"+"<div class='col'>"+status+"<div class='last'>"+current+"</div></div>");
+$("#userInfo").prepend("<div class = 'row'>"+"<div class='col'>"+"<a target='_blank' href="+link+"><img src=' "+logo+"'alt='img/Twitch.png'</a>"+"</div>"+"<div class='col'>"+name+"</div>"+"<div class='col'>"+status+"<div class='last'></div></div>");
 }
 });}// DATA 0 and URL 0
 
@@ -53,7 +52,7 @@ var name= data1.display_name;
 if(name===404){status = "No longer exist!"}
 
 var link = data1.url;
-$("#userInfo").prepend("<div class = 'row'>"+"<div class='col'>"+"<a target='_blank' href="+link+"><img src=' "+logo+"'alt='img/Twitch.png'</a>"+"</div>"+"<div class='col'>"+name+"</div>"+"<div class='col'> "+status+"<div class='last'>"+current+"</div></div>");
+$("#userInfo").prepend("<div class = 'row'>"+"<div class='col'>"+"<a target='_blank' href="+link+"><img src=' "+logo+"'alt='img/Twitch.png'</a>"+"</div>"+"<div class='col'>"+name+"</div>"+"<div class='col'> "+status+"<div class='last'></div></div>");
 });}// DATA 1 and URL 1
 
 
